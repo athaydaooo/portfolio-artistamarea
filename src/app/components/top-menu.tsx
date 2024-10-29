ea599d2b563562bcd6060da1c0515ace5f27e335
athaydaooo <athaydaooo@gmail.com>
2024-10-29 15:37:12 -0300

    initial

---
+++ src/app/components/top-menu.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TopMenu: React.FC = () => {
+    return (
+        <div className="p-4">
+            <span className="text-x1 font-extra-light mb-4 text-left">MAREABEE</span>
+        </div>
+    );
+};
+
+export default TopMenu;
