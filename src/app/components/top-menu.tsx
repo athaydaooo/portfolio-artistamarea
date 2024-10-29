ce4e70d96b4820f6db5fa87dc96c487656e160ad
athaydaooo <athaydaooo@gmail.com>
2024-10-29 15:50:26 -0300

    changes

--- src/app/components/top-menu.tsx
+++ src/app/components/top-menu.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 const TopMenu: React.FC = () => {
     return (
-        <div className="p-4">
-            <span className="text-x1 font-extra-light mb-4 text-left">MAREABEE</span>
+        <header className="fixed top-0 left-0 w-full z-10">
+        <div className="container pl-4 pt-8">
+          <h1 className="text-3xl font-extralight font-serif tracking-widest">ARTISTA MAREA</h1>
         </div>
+      </header>
     );
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div>
       <TopMenu />
-        <Hero />
+      <Hero />
     </div>
   );
 }
