230692d97e14821bd061b20defb1d5b4e43c1e06
athaydaooo <athaydaooo@gmail.com>
2024-11-19 19:29:13 -0300

    chore: added new pictures and implemented

--- src/app/mareabee/page.tsx
+++ src/app/mareabee/page.tsx
@@ -1,42 +1,38 @@
-import Footer from "../../components/sections/footer/Footer.tsx";
-import Gallery from "../../components/sections/gallery/Gallery.tsx";
-import TopMenu from "../../components/sections/top-menu/TopMenu.tsx";
-import AspectRatio from "../../types/aspect-ratio.ts";
+import Gallery from "@/components/sections/gallery/Gallery.tsx";
+import AspectRatio from "@/types/aspect-ratio.ts";
 
 export default function Mareabee() {
   return (
     <>
-      <TopMenu
-        sections={["galeria", "videos", "o ultimo frame", "sobre mim"]}
-      />
-
-      <div className="flex flex-col w-screen h-screen items-center">
+      <div className="flex flex-col w-screen h-[100h] items-center">
         <Gallery
           images={[
-            { src: "/gallery/1.jpg", ratio: AspectRatio.Square },
-            { src: "/gallery/2.jpg", ratio: AspectRatio.ModernLandscape },
-            { src: "/gallery/3.jpg", ratio: AspectRatio.ModernLandscape },
-            { src: "/gallery/4.jpg", ratio: AspectRatio.ModernLandscape },
-            { src: "/gallery/5.jpg", ratio: AspectRatio.ModernLandscape },
-            { src: "/gallery/9.jpg", ratio: AspectRatio.ModernPortrait },
-            { src: "/gallery/7.jpg", ratio: AspectRatio.ModernLandscape },
-            { src: "/gallery/8.jpg", ratio: AspectRatio.ClassicLandscape },
-            { src: "/gallery/9.jpg", ratio: AspectRatio.ClassicLandscape },
-            { src: "/gallery/10.jpg", ratio: AspectRatio.ModernLandscape },
-            { src: "/gallery/11.jpg", ratio: AspectRatio.ModernLandscape },
-            { src: "/gallery/9.jpg", ratio: AspectRatio.ModernLandscape },
-            { src: "/gallery/1.jpg", ratio: AspectRatio.Square },
-            { src: "/gallery/2.jpg", ratio: AspectRatio.ClassicLandscape },
-            { src: "/gallery/3.jpg", ratio: AspectRatio.ModernLandscape },
-            { src: "/gallery/4.jpg", ratio: AspectRatio.ClassicLandscape },
-            { src: "/gallery/9.jpg", ratio: AspectRatio.ModernPortrait },
-            { src: "/gallery/2.jpg", ratio: AspectRatio.ModernLandscape },
-            { src: "/gallery/4.jpg", ratio: AspectRatio.ModernLandscape },
-            { src: "/gallery/4.jpg", ratio: AspectRatio.ModernPortrait },
+            { src: "/mareabee/1.jpg", ratio: AspectRatio.ClassicLandscape },
+            { src: "/mareabee/2.jpg", ratio: AspectRatio.ClassicPortrait },
+            { src: "/mareabee/3.jpg", ratio: AspectRatio.ClassicPortrait },
+            { src: "/mareabee/4.jpg", ratio: AspectRatio.ClassicPortrait },
+            { src: "/mareabee/5.jpg", ratio: AspectRatio.ClassicLandscape },
+            { src: "/mareabee/6.jpg", ratio: AspectRatio.ModernPortrait },
+            { src: "/mareabee/7.jpg", ratio: AspectRatio.ModernLandscape },
+            { src: "/mareabee/8.jpg", ratio: AspectRatio.ClassicLandscape },
+            { src: "/mareabee/9.jpg", ratio: AspectRatio.ModernPortrait },
+            { src: "/mareabee/10.jpg", ratio: AspectRatio.ClassicLandscape },
+            { src: "/mareabee/11.jpg", ratio: AspectRatio.ModernLandscape },
+            { src: "/mareabee/12.jpg", ratio: AspectRatio.ModernLandscape },
+            { src: "/mareabee/13.jpg", ratio: AspectRatio.Square },
+            { src: "/mareabee/14.jpg", ratio: AspectRatio.ClassicLandscape },
+            { src: "/mareabee/15.jpg", ratio: AspectRatio.ClassicLandscape },
+            { src: "/mareabee/16.jpg", ratio: AspectRatio.ClassicLandscape },
+            { src: "/mareabee/17.jpg", ratio: AspectRatio.ClassicLandscape },
+            { src: "/mareabee/18.jpg", ratio: AspectRatio.ClassicLandscape },
+            { src: "/mareabee/19.jpg", ratio: AspectRatio.ModernLandscape },
+            { src: "/mareabee/20.jpg", ratio: AspectRatio.ClassicLandscape },
           ]}
         />
       </div>
-      <Footer />
     </>
   );
 }
+
+/*
+ */
