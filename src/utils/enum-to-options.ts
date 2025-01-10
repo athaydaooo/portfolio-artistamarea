a7249539419b986cd67b8a2f2511fffafc5ed20e
Lucas Athayde <athaydaooo@gmail.com>
2025-01-10 19:27:29 -0300

    fix/unsed util

--- src/utils/enum-to-options.ts
+++
@@ -1,8 +0,0 @@
-const enumToOptions = (enumObj: Record<string, string>) => {
-  return Object.keys(enumObj).map((key) => ({
-    label: key, // Usa a chave do enum como label
-    value: enumObj[key], // Usa o valor do enum como value
-  }));
-};
-
-export default enumToOptions;
