cb517fb2a5e5745507ea29ac6d6baf1d1b2cc98b
Lucas Athayde <athaydaooo@gmail.com>
2025-03-07 15:18:28 -0300

    fix: remove comment

--- src/app/o-ultimo-frame/page.tsx
+++ src/app/o-ultimo-frame/page.tsx
@@ -1,7 +1,3 @@
-/* eslint-disable @next/next/no-async-client-component */
-/* eslint-disable unused-imports/no-unused-vars */
-/* eslint-disable react-hooks/rules-of-hooks */
-
 import { client } from "../../../tina/__generated__/client.ts";
 import OUltimoFramePageBuilder from "./builder.tsx";
 
